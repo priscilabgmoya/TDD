834cb7c2ee24b8c9ce9979cec76e4ccd8e9e9d71
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2024-01-11 17:15:05 -0300

    se agrega fizzbuzz creado desde los test

---
+++ src/fizzBuzz.js
@@ -0,0 +1,19 @@
+/**
+ * Escriba una funcion que al pasarle un numero  
+ * - muestra "fizz" si es multiplo de tres 
+ * - muestra "buzz" si es multipo de cinco
+ * - muestra "fizzBuzz" si es multiplo de tres y cinco 
+ * - muestra el numero si no es multiplo de ninguno 
+ */
+const FizzBuzz = (number) => {
+    if (typeof number !== "number") throw new Error("It isn't a number");
+    if (Number.isNaN(number)) throw new Error("It isn't a number. Is NaN");
+    const multiplies = { 3: "fizz", 5: "buzz" };
+    let output = '';
+    Object.entries(multiplies).forEach(([multiplier, word]) => {
+        if (number % multiplier === 0) return output += word;
+    })
+    return output === '' ? number : output;
+}
+
+export default FizzBuzz; 
