5dc538c8fba7a1cc5d561a143f24b3d61eb1affc
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2024-01-11 22:34:18 -0300

    se agrega ejercicio creado a partir de TDD

---
+++ src/can-reconfigure.js
@@ -0,0 +1,34 @@
+/**
+ * Estamos en la fábrica de Santa Claus 🎅 creando regalos como si no hubiera un mañana.
+ * Pensábamos que no íbamos a llegar pero Jelf Bezos ha tenido una idea genial para aprovechar 
+ * las máquinas y optimizar al máximo la creación de regalos. 🎁
+ * La configuración de las máquinas es un string. Podemos reconfigurarla para que haga otro regalo 
+ * y, para ello, podemos cambiar cada carácter por otro.
+ * Pero tiene limitaciones 🥲: al reemplazar el carácter se debe mantener el orden, no se puede asignar 
+ * al mismo carácter a dos letras distintas (pero sí a si mismo) y, claro, la longitud del string debe ser el mismo.
+ * Necesitamos una función que nos diga si podemos reconfigurar una máquina para que de un regalo pueda pasar
+ *  a fabricar otro según las reglas mencionadas.
+ */
+const canReconfigure = (from, to) =>{
+    if( typeof from !== "string") throw new Error("from isn't a string")
+    if(typeof to !== "string")  throw new Error("to isn't a string"); 
+
+    const sameLength = from.length === to.length;
+    if(!sameLength) return false; 
+
+    const hasSameUniqueLetters = new Set(from).size === new Set(to).size; 
+    if(!hasSameUniqueLetters) return false; 
+
+    const transformations ={}
+    for (let index = 0; index < from.length; index++) {
+        const fromLetter = from[index];
+        const toLetter = to[index]; 
+
+        const storedLetter = transformations[fromLetter]; 
+        if(storedLetter && storedLetter !== toLetter) return false;
+        transformations[fromLetter] = toLetter; 
+    }
+    return true; 
+}
+
+export default canReconfigure; 
---
+++ test/can-reconfigure.test.js
@@ -0,0 +1,32 @@
+import { describe, it, expect } from "vitest";
+import canReconfigure from "../src/can-reconfigure";
+
+describe("canReconfigure", ()=> {
+    it("should be a function", ()=>{
+        expect(canReconfigure).toBeTypeOf("function"); 
+    }); 
+    it("should throw if first parameter is missing" , ()=> {
+        expect(()=> canReconfigure()).throw(); 
+    }); 
+    it("should throw if first parameter is not a string" , ()=> {
+        expect(()=> canReconfigure(2)).throw(); 
+    });
+    it("should throw if second parameter is not a string" , ()=> {
+        expect(()=> canReconfigure("a")).throw(); 
+    }); 
+    it("should return a boolean", () => {
+        expect(canReconfigure('a','b')). toBeTypeOf("boolean"); 
+    }); 
+    it("should return false if strings provided have different length", () => {
+        expect(canReconfigure('assssss','b')).toBe(false); 
+    }); 
+    it("should return false if strings provided have different length even with same unique letters", () => {
+        expect(canReconfigure('aab','ab')).toBe(false); 
+    });
+    it("should return false if strings provided have different number of unique letters", () => {
+        expect(canReconfigure('abc','ddd')).toBe(false); 
+    }); 
+    it("should return false if strings provided have different order of transformation", () => {
+        expect(canReconfigure('XBOX','XXBO')).toBe(false); 
+    }); 
+})
